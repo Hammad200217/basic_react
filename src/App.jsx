0d18ec1e770d8ae28afb003f17b4e053cdaf8ca9
Hammad Ali <hammadali78056@gmail.com>
2024-08-17 08:20:53 +0500

    error clear

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Tittle from './Tittle.jsx';
+import Item from './Item.jsx';
+import React from 'react'
+function App() {
+return (
+  <div>
+<Item/>
+ </div>
+);
+}
+
+export default App
---
+++ src/Tittle.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Tittle() {
+  return (
+    <div>
+        <h1>I am Tittle</h1>
+    </div>
+  )
+}
+
+export default Tittle
